223ecc6902788390ad5a9f7eafda6912e256cfa8
Ericka Arguedas <ericka.arguedas@gmail.com>
2020-11-26 00:28:53 -0800

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
-import { ScrollView, Text, FlatList, View } from 'react-native';
-import { Card, ListItem } from 'react-native-elements';
-import { connect } from 'react-redux';
-import { baseUrl } from '../shared/baseUrl';
+import React, { Component } from "react";
+import { ScrollView, Text, FlatList, View } from "react-native";
+import { Card, ListItem } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
-const mapStateToProps = state => {
-    return {
-        partners: state.partners
-    };
+const mapStateToProps = (state) => {
+  return {
+    partners: state.partners,
+  };
 };
 
 const Mission = () => {
@@ -29,7 +30,6 @@ const Mission = () => {
 };
 
 class About extends Component {
-
   static navigationOptions = {
     title: "About",
   };
@@ -40,23 +40,43 @@ class About extends Component {
         <ListItem
           title={item.name}
           subtitle={item.description}
-          leftAvatar={{source: {uri: baseUrl + item.image}}}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
         />
       );
     };
 
-    return (
-      <ScrollView>
-        <Mission />
-        <Card title="Community Partners">
-          <FlatList 
-              data={this.props.partners.partners}
-              renderItem={renderPartner}
-              keyExtractor={item => item.id.toString()}
-          />
-        </Card>
-      </ScrollView>
-    );
+        if (this.props.partners.isLoading) {
+          return (
+            <ScrollView>
+              <Mission />
+              <Card title="Community Partners">
+                <Loading />
+              </Card>
+            </ScrollView>
+          );
+        }
+        if (this.props.partners.errMess) {
+          return (
+            <ScrollView>
+              <Mission />
+              <Card title="Community Partners">
+                <Text>{this.props.partners.errMess}</Text>
+              </Card>
+            </ScrollView>
+          );
+        }
+        return (
+          <ScrollView>
+            <Mission />
+            <Card title="Community Partners">
+              <FlatList
+                data={this.props.partners.partners}
+                renderItem={renderPartner}
+                keyExtractor={(item) => item.id.toString()}
+              />
+            </Card>
+          </ScrollView>
+        );
   }
 }
 
